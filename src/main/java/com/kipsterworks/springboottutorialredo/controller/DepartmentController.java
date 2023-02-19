66df2a911997599979053b39916dab979b82884b
Ryan Jacobo <ryanjacobo@yahoo.com>
2023-02-18 19:10:02 -0500

    added error handling and mysql config

--- src/main/java/com/kipsterworks/springboottutorialredo/controller/DepartmentController.java
+++ src/main/java/com/kipsterworks/springboottutorialredo/controller/DepartmentController.java
@@ -1,11 +1,12 @@
 package com.kipsterworks.springboottutorialredo.controller;
 
 import com.kipsterworks.springboottutorialredo.entity.Department;
+import com.kipsterworks.springboottutorialredo.error.DepartmentNotFoundException;
 import com.kipsterworks.springboottutorialredo.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -15,5 +16,31 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList() {
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentListId(@PathVariable("id")Long departmentId) throws DepartmentNotFoundException {
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id")Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted successfully!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment(@PathVariable("id") Long departmentId, @RequestBody Department department){
+        return departmentService.updateDepartment(departmentId, department);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name")String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
     }
 }
--- src/main/java/com/kipsterworks/springboottutorialredo/service/DepartmentService.java
+++ src/main/java/com/kipsterworks/springboottutorialredo/service/DepartmentService.java
@@ -1,7 +1,20 @@
 package com.kipsterworks.springboottutorialredo.service;
 
 import com.kipsterworks.springboottutorialredo.entity.Department;
+import com.kipsterworks.springboottutorialredo.error.DepartmentNotFoundException;
+
+import java.util.List;
 
 public interface DepartmentService {
     public Department saveDepartment(Department department);
+
+    public List<Department> fetchDepartmentList();
+
+    public Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+
+    public void deleteDepartmentById(Long departmentId);
+
+    public Department updateDepartment(Long departmentId, Department department);
+
+    public Department fetchDepartmentByName(String departmentName);
 }
